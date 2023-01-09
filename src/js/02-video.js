1b178fe33cd07987d3b1edfd6222e03951a6567d
Dasha Yarkovska <yarkovska11@gmail.com>
2023-01-09 20:46:53 +0200

    add parcel-template

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+// const throttle = require('lodash.throttle');
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
