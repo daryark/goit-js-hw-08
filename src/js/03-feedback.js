3bdbf8f4231d3deef073b92f2bb2835ce60fcc80
Dasha Yarkovska <yarkovska11@gmail.com>
2023-01-11 10:19:16 +0200

    fix task 3 (clear on submit)

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,12 @@
 const throttle = require('lodash.throttle');
 
 const formEl = document.querySelector('.feedback-form');
-const userDataObj = {};
+const LS_KEY = 'feedback-form-state';
+let userDataObj = {};
 
 function getDataFromLocalStorage() {
   try {
-    const localStorageData = JSON.parse(localStorage.getItem('userDataObj'));
+    const localStorageData = JSON.parse(localStorage.getItem(LS_KEY));
 
     if (localStorageData === null) {
       return;
@@ -21,8 +22,17 @@ function getDataFromLocalStorage() {
 getDataFromLocalStorage();
 
 function onChange(e) {
+  localStorage.setItem(LS_KEY, JSON.stringify(userDataObj));
+
+  userDataObj = JSON.parse(localStorage.getItem(LS_KEY));
   userDataObj[e.target.name] = e.target.value;
-  localStorage.setItem('userDataObj', JSON.stringify(userDataObj));
+
+  console.log(JSON.parse(localStorage.getItem(LS_KEY)));
+}
+
+function clearForm() {
+  localStorage.clear();
 }
 
 formEl.addEventListener('input', throttle(onChange, 500));
+formEl.addEventListener('submit', clearForm);
